bbb04a64fcf7ba64063464d5137452e29eb2194f
Vanessamae23 <vanessamae23@gmail.com>
2022-01-16 11:27:12 +0700

    Fourth commit - added the scorebord

--- challenges/primary-challenges.js
+++ challenges/primary-challenges.js
@@ -1,4 +1,5 @@
-
+var scoreboard = 0
+var videoOn = document.querySelector(".video")
 let currentNumber = 0;
 let list = [ {
     question: "1. 325 children attended a concert at a school auditorium. 4/5 of the boys and 3/4 of the girls left the auditorium at the end of the concert. 29 more boys than girls remained in the auditorium. How many girls attended the concert?",
@@ -31,15 +32,23 @@ function submit() {
 }
 
 function isCorrect() {
+    /* the new scoreboard function */
+    if (window.getComputedStyle(videoOn).display === "none") {
+            scoreboard++;
+          } 
+        console.log(scoreboard) 
     if (currentNumber < (list.length - 1)) {
-
         currentNumber++;
+        
         questionUpdate();
         $(".video").css("display", "none")
         $(".wrong-sentence").css("visibility", "hidden")
     } else {
         $(".bodyhead").css("display", "none")
-        $("footer").before("<p>Nice try</p>")
+        var text = document.createElement("p")
+        text.style.margin = "20px";
+        text.innerHTML = `Congratulations, your score is ${scoreboard} out of ${list.length}`
+        $("footer").before(text)
         }
     }
 
