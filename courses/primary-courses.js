4496277f0116567bbde2f5a7cda343c32480778e
Vanessamae23 <vanessamae23@gmail.com>
2022-01-17 09:40:14 +0700

    Added the speed video

--- courses/primary-courses.js
+++ courses/primary-courses.js
@@ -3,3 +3,9 @@ $("#circles").click(function() {
 
     $("#circles").before('<iframe class="video" width="512" height="315" src="https://www.youtube.com/embed/VGNSVfytyT8" allowfullscreen></iframe>')
 })
+
+$("#speed").click(function() {
+    $("#speed").css("display", "none")
+
+    $("#speed").before('<iframe class="video" width="512" height="315" src="https://www.youtube.com/embed/LeImkWUPH_E" allowfullscreen></iframe>')
+})
